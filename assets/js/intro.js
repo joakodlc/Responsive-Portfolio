e7f7c8a8ce3e9f583500ea06de0acfc61a3873c8
joakodlc <84253573+joakodlc@users.noreply.github.com>
2022-07-24 19:38:31 -0300

    Update intro.js

--- assets/js/intro.js
+++ assets/js/intro.js
@@ -1,29 +1,6 @@
+const fadeOut = () => {
+    const loaderWrapper = document.querySelector('.loader__wrap');
+    loaderWrapper.classList.add('fade');
+}
 
-let intro = document.querySelector('.intro');
-let logo = document.querySelector('.logo-header');
-let logoSpan = document.querySelectorAll('.logo');
-
-window.addEventListener('DOMContentLoaded', () => {
-
-    setTimeout(() => {
-        logoSpan.forEach((span, idx) => {
-            setTimeout(() => {
-                span.classList.add('active');
-            }, (idx + 1) * 400)
-        });
-
-        setTimeout(() => {
-            logoSpan.forEach((span, idx) => {
-
-                setTimeout(() => {
-                    span.classList.remove('active');
-                    span.classList.add('fade');
-                }, (idx + 1) * 50)
-            })
-        }, 2000);
-
-        setTimeout(() => {
-            intro.style.top = '-100vh'
-        }, 2300);
-    })
-})
+window.addEventListener('load', fadeOut);
